3deaedf996fb7771be398a1849de439c6af35119
pejoyjp <75529294+pejoyjp@users.noreply.github.com>
2023-10-28 16:41:37 +0100

    Delete venv directory

--- venv/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-# @app.route("/listdrivers")
-# def listdrivers():
-#     connection = getCursor()
-#     connection.execute("SELECT * FROM driver;")
-#     driverList = connection.fetchall()
-#     print(driverList)
-#     return render_template("driverlist.html", driver_list = driverList)    
